227c4f1ff85417f5ae217342239f5d3f2cd6afdb
martin <xorg62@gmail.com>
2008-06-09 20:21:09 +0200

    add OC for 12h format

--- clock.c
+++ clock.c
@@ -1,39 +1,39 @@
-/*      clock.c    - TTY-Clock main function
+/*		clock.c	 - TTY-Clock main function
  *
- *      Copyright © 2008 Duquesnoy Martin <xorg62@gmail.com>
+ *		Copyright © 2008 Duquesnoy Martin <xorg62@gmail.com>
  *
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
+ *		This program is free software; you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation; either version 2 of the License, or
+ *		(at your option) any later version.
  *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details.
  *
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program; if not, write to the Free Software
+ *		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *		MA 02110-1301, USA.
  */
 
 #include <stdlib.h>
 #include <time.h>
 #include <ncurses.h>
 #include <unistd.h>
-#include <getopt.h>    
+#include <getopt.h>	 
 
 #define printh() printf("tty-clock usage : tty-clock -[option] -[option] <arg>\n\n\
-  -s, --second        Show seconds\n\
-  -t, --tw            Set the hour in 12h format\n\
-  -x  <integer>       Set the clock to X\n\
-  -y  <integer>       Set the clock to Y\n\
-  -v, --version       Show tty-clock version\n\
-  -i, --info          Show some info about tty-clock\n\
-  -h, --help          Show this page\n\n\
+  -s, --second		  Show seconds\n\
+  -t, --tw				Set the hour in 12h format\n\
+  -x  <integer>		 Set the clock to X\n\
+  -y  <integer>		 Set the clock to Y\n\
+  -v, --version		 Show tty-clock version\n\
+  -i, --info			 Show some info about tty-clock\n\
+  -h, --help			 Show this page\n\n\
 Try keypad arrow for move the clock ;)\n");\
-                      
+							 
 
 #define LGNUM 30
 #define XLENGTH 5
@@ -45,16 +45,16 @@ Try keypad arrow for move the clock ;)\n");\
 /* *************** */
 
 static const long number[10][LGNUM] = {
-    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 0 */
-    {0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 1 */
-    {1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1}, /* 2 */
-    {1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}, /* 3 */
-    {1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 4 */
-    {1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}, /* 5 */
-    {1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 6 */
-    {1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 7 */
-    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 8 */
-    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}  /* 9 */
+	 {1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 0 */
+	 {0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 1 */
+	 {1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1}, /* 2 */
+	 {1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}, /* 3 */
+	 {1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 4 */
+	 {1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}, /* 5 */
+	 {1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 6 */
+	 {1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1}, /* 7 */
+	 {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1}, /* 8 */
+	 {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1}  /* 9 */
 };
 
 int hour[2];
@@ -76,16 +76,16 @@ time_t lt;
 
 void 
 start(void) {
-    initscr ();    
-    noecho ();
-    keypad (stdscr, TRUE);    
-    start_color ();
-    refresh();
-    if (use_default_colors() == OK)
-    bg = -1;
-    init_pair(1,COLOR_GREEN, COLOR_GREEN);
-    init_pair(2, bg, bg);
-    curs_set(0);
+	 initscr ();	 
+	 noecho ();
+	 keypad (stdscr, TRUE);	 
+	 start_color ();
+	 refresh();
+	 if (use_default_colors() == OK);
+	 bg = -1;
+	 init_pair(1,COLOR_GREEN, COLOR_GREEN);
+	 init_pair(2, bg, bg);
+	 curs_set(0);
 }
 
 /* **************************** */
@@ -94,32 +94,32 @@ start(void) {
 
 void 
 print_number(int num, int x, int y) {
-    int i,u,count=0;
-    char c;
-    int tab[LGNUM];
-    int lx=x;
-    int ly=y;
-    
-    for (u = 0; u < LGNUM; ++u){
-        tab[u] = number[num][u];
-    }
+	 int i,u,count=0;
+	 char c;
+	 int tab[LGNUM];
+	 int lx=x;
+	 int ly=y;
+	 
+	 for (u = 0; u < LGNUM; ++u){
+		  tab[u] = number[num][u];
+	 }
 
-    for (i = 0; i < LGNUM; ++i) {
-        c =    (tab[i] != 1) ? 2 : 1;
-     
+	 for (i = 0; i < LGNUM; ++i) {
+		  c =	 (tab[i] != 1) ? 2 : 1;
+	  
 		if(count == 6){ 
-            ++lx;
-            ly=y;
-            count = 0;
-        }
+			++lx;
+			ly=y;
+			count = 0;
+		}
 
-        move(lx, ly);
-        attron(COLOR_PAIR(c));
-        printw(" ");
-        attroff(COLOR_PAIR(c));
-        ++ly;
-        ++count;
-    }
+		move(lx, ly);
+		attron(COLOR_PAIR(c));
+		printw(" ");
+		attroff(COLOR_PAIR(c));
+		++ly;
+		++count;
+	 }
 }
 
 /* ******************************** */
@@ -128,45 +128,45 @@ print_number(int num, int x, int y) {
 
 void  
 arrange_clock(int h1, int h2, 
-              int m1, int m2, 
-              int s1, int s2) {
-    int i;
+			  int m1, int m2, 
+			  int s1, int s2) {
+	 int i;
 
-    for(i = defy + DEPTHB; i < defy + YLENGTH - SCHANGE; ++i){
-        mvaddch(defx + DEPTHB, i, ACS_HLINE);
-        mvaddch(defx + XLENGTH, i,ACS_HLINE);
-    }
+	 for(i = defy + DEPTHB; i < defy + YLENGTH - SCHANGE; ++i){
+		  mvaddch(defx + DEPTHB, i, ACS_HLINE);
+		  mvaddch(defx + XLENGTH, i,ACS_HLINE);
+	 }
 
-    for (i = defx + DEPTHB; i < defx + XLENGTH; ++i){
-        mvaddch(i, defy + DEPTHB, ACS_VLINE);
-        mvaddch(i, defy + YLENGTH - SCHANGE, ACS_VLINE);
-    }
-    
-    mvaddch(defx + DEPTHB, defy + DEPTHB, ACS_ULCORNER);
-    mvaddch(defx + XLENGTH, defy + DEPTHB, ACS_LLCORNER);
-    mvaddch(defx + DEPTHB, defy + YLENGTH - SCHANGE, ACS_URCORNER);
-    mvaddch(defx + XLENGTH, defy + YLENGTH - SCHANGE, ACS_LRCORNER);
+	 for (i = defx + DEPTHB; i < defx + XLENGTH; ++i){
+		  mvaddch(i, defy + DEPTHB, ACS_VLINE);
+		  mvaddch(i, defy + YLENGTH - SCHANGE, ACS_VLINE);
+	 }
+	 
+	 mvaddch(defx + DEPTHB, defy + DEPTHB, ACS_ULCORNER);
+	 mvaddch(defx + XLENGTH, defy + DEPTHB, ACS_LLCORNER);
+	 mvaddch(defx + DEPTHB, defy + YLENGTH - SCHANGE, ACS_URCORNER);
+	 mvaddch(defx + XLENGTH, defy + YLENGTH - SCHANGE, ACS_LRCORNER);
 
-    print_number(h1, defx, defy);
-    print_number(h2, defx, defy + 7);
-    
-    attron(COLOR_PAIR(1));
-    mvaddstr(defx + 1, defy + 15,"  ");
-    mvaddstr(defx + 3, defy + 15,"  ");
-    attroff(COLOR_PAIR(1));
+	 print_number(h1, defx, defy);
+	 print_number(h2, defx, defy + 7);
+	 
+	 attron(COLOR_PAIR(1));
+	 mvaddstr(defx + 1, defy + 15,"  ");
+	 mvaddstr(defx + 3, defy + 15,"  ");
+	 attroff(COLOR_PAIR(1));
 
-    print_number(m1, defx, defy + 19);
-    print_number(m2, defx, defy + 26);
+	 print_number(m1, defx, defy + 19);
+	 print_number(m2, defx, defy + 26);
 
-    if(enable_sec){
-        attron(COLOR_PAIR(1));
-        mvaddstr(defx + 1, defy + 34,"  ");
-        mvaddstr(defx + 3, defy + 34,"  ");
-        attroff(COLOR_PAIR(1));
-    
-        print_number(s1, defx, defy + 38);
-        print_number(s2, defx, defy + 45);
-    }
+	 if(enable_sec){
+		  attron(COLOR_PAIR(1));
+		  mvaddstr(defx + 1, defy + 34,"  ");
+		  mvaddstr(defx + 3, defy + 34,"  ");
+		  attroff(COLOR_PAIR(1));
+	 
+		  print_number(s1, defx, defy + 38);
+		  print_number(s2, defx, defy + 45);
+	 }
 }
 
 
@@ -176,44 +176,44 @@ arrange_clock(int h1, int h2,
 
 void
 check_key(void) {
-    int c;
-    c = getch();
+	 int c;
+	 c = getch();
 
-    switch(c) {
-        case KEY_UP:
-        case 'k':
-        case 'K':
-            if(defx > 1) {
-                --defx;
-                clear();
-            }
-            break;
-        case KEY_DOWN:
-        case 'j':
-        case 'J':
-            ++defx;
-            clear();
-            break;
-        case KEY_LEFT:
-        case 'h':
-        case 'H':
-            if(defy > 1) {
-                --defy;
-                clear();
-            }
-            break;
-        case KEY_RIGHT:    
-        case 'l':
-        case 'L':
-            ++defy;
-            clear();
-            break;
+	 switch(c) {
+		  case KEY_UP:
+		  case 'k':
+		  case 'K':
+				if(defx > 1) {
+					 --defx;
+					 clear();
+				}
+				break;
+		  case KEY_DOWN:
+		  case 'j':
+		  case 'J':
+				++defx;
+				clear();
+				break;
+		  case KEY_LEFT:
+		  case 'h':
+		  case 'H':
+				if(defy > 1) {
+					 --defy;
+					 clear();
+				}
+				break;
+		  case KEY_RIGHT:	 
+		  case 'l':
+		  case 'L':
+				++defy;
+				clear();
+				break;
 		case 'q':
 		case 'Q':
 			endwin();
 			exit(EXIT_SUCCESS);
 			break;
-    }
+	 }
 }
 
 /* ********************* */
@@ -222,15 +222,15 @@ check_key(void) {
 
 void
 get_time(void) {
-    int i;
+	int i;
 	int ihour;
-    tm = localtime(&lt);
-    lt = time(NULL);
+	tm = localtime(&lt);
+	lt = time(NULL);
 
 	ihour = tm->tm_hour;
 
 	ihour = (enable_tw && ihour > 12) ? ihour - 12 : ihour;
-    ihour = (enable_tw && !ihour) ? 12 : ihour;
+	ihour = (enable_tw && !ihour) ? 12 : ihour;
 
 	hour[0] = ihour / 10;
 	hour[1] = ihour % 10;
@@ -238,11 +238,10 @@ get_time(void) {
 	min[0] = tm->tm_min / 10;
 	min[1] = tm->tm_min % 10;
 
-    if(enable_sec);
-    
+	if(enable_sec) {
 		sec[0] = tm->tm_sec / 10;
 		sec[1] = tm->tm_sec % 10;
-    
+	}
 }
 
 /* *********** */
@@ -251,12 +250,12 @@ get_time(void) {
 
 void 
 run(void) {
-    get_time();
-    arrange_clock(hour[0], hour[1],
-                  min[0], min[1],
-                  sec[0], sec[1]);
-    refresh();
-    halfdelay(1);
+	 get_time();
+	 arrange_clock(hour[0], hour[1],
+					min[0], min[1],
+					sec[0], sec[1]);
+	 refresh();
+	 halfdelay(1);
 }
 
 /* ************ */
@@ -265,71 +264,70 @@ run(void) {
 
 int 
 main(int argc,char **argv) {
-    int c;
-    
-    static struct option long_options[] ={
-        {"help",    0, NULL, 'h'},
-        {"version", 0, NULL, 'v'},
-        {"info",    0, NULL, 'i'},
-        {"second",  0, NULL, 's'},
+	 int c;
+	 
+	static struct option long_options[] ={
+		{"help",	 0, NULL, 'h'},
+		{"version", 0, NULL, 'v'},
+		{"info",	 0, NULL, 'i'},
+		{"second",  0, NULL, 's'},
 		{"twelve",	0, NULL, 't'},
-        {NULL,      0, NULL, 0}
-       };
+		{NULL,		0, NULL, 0}
+		};
 
-     while ((c = getopt_long(argc,argv,"tx:y:vsih",
-                     long_options,NULL)) != -1) {
-        switch(c) {
-            case 'h':
-            default:
-                printh();
-                exit(EXIT_SUCCESS);
-                break;
-            case 'i':
-                printf("TTY-Clock ,Martin Duquesnoy© (xorg62@gmail.com)\n");
-                exit(EXIT_SUCCESS);
-                break;
-            case 'v':
-                printf("TTY-Clock v0.1alpha2\n");
-                exit(EXIT_SUCCESS);
-                break;
-            case 'x':
-                if(atoi(optarg)<0){
-                    exit(EXIT_FAILURE);
-                } else {
-                    defx = atoi(optarg) + 1;
-                }
-                break;
-            case 'y':
-                if(atoi(optarg)<0){
-                    exit(EXIT_FAILURE);
-                } else {
-                    defy = atoi(optarg) + 1;
-                }
-                break;
-            case 's':
-                SCHANGE = 0;
-                enable_sec = 1;
-                break;
-			case 't':
-				enable_tw = 1;
-				break;
-        }
-     }
+	  while ((c = getopt_long(argc,argv,"tx:y:vsih",
+			long_options,NULL)) != -1) {
+		switch(c) {
+			case 'h':
+			default:
+				 printh();
+				 exit(EXIT_SUCCESS);
+				 break;
+			case 'i':
+				 printf("TTY-Clock ,Martin Duquesnoy© (xorg62@gmail.com)\n");
+				 exit(EXIT_SUCCESS);
+				 break;
+			case 'v':
+				 printf("TTY-Clock v0.1alpha2\n");
+				 exit(EXIT_SUCCESS);
+				 break;
+			case 'x':
+				 if(atoi(optarg)<0){
+					  exit(EXIT_FAILURE);
+				 } else {
+					  defx = atoi(optarg) + 1;
+				 }
+				 break;
+			case 'y':
+				 if(atoi(optarg)<0){
+					  exit(EXIT_FAILURE);
+				 } else {
+					  defy = atoi(optarg) + 1;
+				 }
+				 break;
+			case 's':
+				 SCHANGE = 0;
+				 enable_sec = 1;
+				 break;
+		case 't':
+			enable_tw = 1;
+			break;
+	  }
+  }
 
-    start();
+	 start();
 
-/* first time init */    
+/* first time init */	 
 
-    run();
+ run();
 
-/* endless loop */
+ /* endless loop */
 
-    while(1) {
-        usleep(10000);
-        check_key();
-        run();
-    }
-
-    endwin();
-    return 0;
+ while(1) {
+	  usleep(10000);
+	  check_key();
+	  run();
+ }
+	 endwin();
+	 return 0;
 }
